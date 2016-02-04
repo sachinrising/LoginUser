221b9a4f8546f00829ef52d40566d4c70be6670f
Sachin Srivastava <sachinrising@gmail.com>
2016-02-04 09:17:32 +0530

    Added remember me to login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+  
   has_secure_password
   
   validates :email, presence: true, length: {minimum: 5}, uniqueness: true
   validates :password, presence: true, length: {minimum: 5, maximum: 15}
+
+  attr_accessor :remember_digest
+
+def newToken
+	SecureRandom.urlsafe_base64
+end
+
+def remember_me
+	@token = Password.create(newToken)
+    self.remember_digest = @token
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,13 @@
 ActiveRecord::Schema.define(version: 20160203163429) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "remeber_digest"
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "remeber_digest",  limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
